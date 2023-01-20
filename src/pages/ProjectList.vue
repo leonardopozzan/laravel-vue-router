b82e8afea0090e6cd1f21f8b39dc6eaf1dfd617b
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-20 17:14:40 +0100

    inserito pagination

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="cards">
         <CardComponent v-for="(project,i) in projects" :key="i" :project="project"></CardComponent>
+        <button @click="getPage('prev')" :disabled="currentPage == 1">prev</button>
+        <button v-for="n in pagination.last_page" @click="getPage(n)" :disabled="currentPage == n">{{ n }}</button>
+        <button @click="getPage('next')" :disabled="currentPage == pagination.last_page">next</button>
     </div>
 </template>
 
@@ -13,14 +16,31 @@ export default {
         return {
             store,
             projects: [],
+            currentPage : 1,
+            pagination : {last_page : 1}
         };
     },
     methods: {
         getProjects() {
-            axios.get(`${store.apiUrl}/projects`).then((response) => {
+            const data = {
+                params: {'page' : this.currentPage}
+            }
+            axios.get(`${store.apiUrl}/projects`,data).then((response) => {
+                // console.log(response.data.results)
                 // console.log(response.data.results);
-                this.projects = response.data.results;
+                this.projects = response.data.results.data;
+                this.pagination = response.data.results;
             });
+        },
+        getPage(page){
+            if(page == 'next'){
+                this.currentPage++;
+            }else if (page == 'prev'){
+                this.currentPage--;
+            }else{
+                this.currentPage = page;
+            }
+            this.getProjects();
         }
     },
     mounted() {
@@ -36,5 +56,8 @@ export default {
     align-items: flex-start;
     flex-wrap: wrap;
     
+}
+button{
+    padding: 0.5rem;
 }
 </style>
