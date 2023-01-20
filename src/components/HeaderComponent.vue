e3d902ad9f09c9b909ae4b043ddc571e0c2e1567
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2023-01-20 17:34:42 +0100

    inserito logo

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -2,7 +2,7 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="/img/logo.png" alt="" id="logo"></a>
+                <a class="navbar-brand" href="#"><img src="/img/logo3.png" alt="" id="logo"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
